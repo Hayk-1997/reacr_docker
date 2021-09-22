be549a5cab81ea10bc7ca02e814e97de854e0115
Hayk Abrahamyan <abrahamyanhayk01@gmail.com>
2021-09-23 00:09:30 +0400

    Set up jest enzyme testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it("renders without crashing", () => {
+  shallow(<App />);
+});
+
+it("renders Account header", () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h1>Display Active Users Account Details</h1>;
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
